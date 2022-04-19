822abb3b30270e6fa765aa9dcf54ce06e4261ee9
Artem Bordiuzha <halp3ars@gmail.com>
2022-04-19 17:40:35 +0300

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,26 @@
-import parser.ParseConfiguration;
+import dto.IpDto;
+import parser.JsonFileParser;
+import parser.JsonUrlParser;
+import parser.ParseStrategy;
+import writer.WriteFile;
 
 import java.io.IOException;
 
 public class Main {
 
-    private static final ParseConfiguration PARSE_CONFIGURATION = new ParseConfiguration();
+    private static final String TARGET_FILE = "/Users/halpears/Desktop/ip-info.txt";
 
     public static void main(String[] args) throws IOException {
-        PARSE_CONFIGURATION.parseWay();
+        if (args[0].equals("url")) {
+            ParseStrategy strategy = new JsonUrlParser();
+            IpDto ipDto = strategy.parseFromLink();
+            WriteFile.writeJsonObject(TARGET_FILE, ipDto.getIp());
+        } else if (args[0].equals("file")) {
+            ParseStrategy strategy = new JsonFileParser();
+            IpDto ipDto = strategy.parseFromLink();
+            WriteFile.writeJsonObject(TARGET_FILE, ipDto.getIp());
+        } else {
+            System.out.println("Strategy not chosen");
+        }
     }
 }
