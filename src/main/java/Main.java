52c6709574819d5bb8ddfa8f83523a0122101b9a
Artem Bordiuzha <halp3ars@gmail.com>
2022-04-20 15:01:42 +0300

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,33 +5,30 @@ import parser.ParseStrategy;
 import writer.WriteFile;
 
 import java.io.IOException;
-import java.net.URL;
 
 public class Main {
 
     private static final String TARGET_FILE = "/Users/halpears/Desktop/ip-info.txt";
     public static IpDto ipDto = new IpDto();
     public static ParseStrategy strategy;
-
-    public enum arguments {
-        FILE("file"), URL("url");
-        private String argument = null;
-        arguments(String argument) {
-            this.argument = argument;
-        }
-        public String getArgument() {
-            return argument;
-        }
+    public enum Arguments {
+        FILE, URL
     }
+
     public static void main(String[] args) throws IOException {
-        if (args[0].equals(arguments.FILE.getArgument())) {
-            strategy = new JsonFileParser();
-        } else if (args[0].equals(arguments.URL.getArgument())) {
-            strategy = new JsonUrlParser();
-        } else {
-            System.out.println("Strategy not correct, only 'file' or 'url'");
+        try {
+            if (args[0].equals(Arguments.FILE.name())) {
+                strategy = new JsonFileParser();
+            } else if (args[0].equals(Arguments.URL.name())) {
+                strategy = new JsonUrlParser();
+            } else {
+                System.out.println("Argument not correct, only 'FILE' or 'URL'");
+            }
+            ipDto = strategy.parseFromLink();
+            WriteFile.writeJsonObject(TARGET_FILE, ipDto.getIp());
+        } catch (NullPointerException | ArrayIndexOutOfBoundsException exception) {
+            System.out.println("Error " + exception.getMessage());
+            exception.printStackTrace();
         }
-        ipDto = strategy.parseFromLink();
-        WriteFile.writeJsonObject(TARGET_FILE, ipDto.getIp());
     }
 }
