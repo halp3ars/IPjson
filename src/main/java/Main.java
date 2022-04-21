1722f7b5885db698bb199c198e618f87a7808696
Artem Bordiuzha <halp3ars@gmail.com>
2022-04-21 15:08:29 +0300

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,14 @@ import java.io.IOException;
 public class Main {
 
     private static final String TARGET_FILE = "/Users/halpears/Desktop/ip-info.txt";
-    private static IpDto ipDto = new IpDto();
     private final static TypeStrategy TYPE_STRATEGY = new TypeStrategy();
 
     public static void main(String[] args) throws IOException {
         if (args.length > 0) {
-            ipDto = TYPE_STRATEGY.getStrategy(args[0]).parseFromLink();
+            IpDto ipDto = TYPE_STRATEGY.getStrategy(args[0]).parseFromLink();
             WriteFile.writeJsonObject(TARGET_FILE, ipDto.getIp());
         } else {
-            System.out.println("Arguments not found");
+            System.out.println("Arguments not found, correct arguments are :" + TYPE_STRATEGY.getStrategyKeySet());
         }
     }
 
--- src/main/java/TypeStrategy.java
+++ src/main/java/TypeStrategy.java
@@ -1,19 +1,28 @@
 import parser.JsonFileParser;
 import parser.JsonUrlParser;
 import parser.ParseStrategy;
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class TypeStrategy {
     private final Map<String, ParseStrategy> STRATEGY_MAP = new HashMap<>();
 
-    public TypeStrategy() {
-        STRATEGY_MAP.put("file", new JsonFileParser());
-        STRATEGY_MAP.put("url", new JsonUrlParser());
+    public TypeStrategy() throws NullPointerException{
+        try {
+            STRATEGY_MAP.put("file", new JsonFileParser());
+            STRATEGY_MAP.put("url", new JsonUrlParser());
+        }catch (NullPointerException exception){
+            System.out.println("Correct arguments are : " + STRATEGY_MAP.keySet());
+        }
+    }
+
+    public Set<String> getStrategyKeySet() {
+        return STRATEGY_MAP.keySet();
     }
 
     public ParseStrategy getStrategy(String strategy) {
         return STRATEGY_MAP.get(strategy);
     }
+
 }
